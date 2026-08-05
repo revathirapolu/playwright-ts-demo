4246cafe1f5390631bfafa9c63f29ac8dd509dfd
Revathi  Rapolu <rapolu.revathi@gmail.com>
2026-08-05 16:17:16 +0100

    initialize Playwright + TypeScript project

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30_000,
+  retries: 1,
+  use: {
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on', //'on-first-retry'
+    /* Base URL to use in actions like `await page.goto('')`. */
+    baseURL: 'https://opensource-demo.orangehrmlive.com'
+  },
+  reporter: 'html',
+  projects: [
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'WebKit',
+      use: { ...devices['Desktop Safari'] }
+    }
+  ]
+});
