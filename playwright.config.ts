df0f05b9ab60f222f174b3c90ad522e64fcb8d12
Revathi Rapolu <rapolu.revathi@gmail.com>
2026-08-13 11:09:05 +0100

    Set Playwright to run in headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   timeout: 30_000,
   retries: 1,
   use: {
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on', //'on-first-retry'
