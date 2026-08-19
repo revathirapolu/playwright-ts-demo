d38a981c26440b1e0251ba2292cba7f43e9db290
Revathi Rapolu <rapolu.revathi@gmail.com>
2026-08-19 11:07:37 +0100

    Fix reporter configuration format in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://opensource-demo.orangehrmlive.com'
   },
-  reporter: ['html',{open: 'never'}],
+  reporter: [['html',{open: 'never'}]],
   projects: [
     {
       name: 'Chromium',
